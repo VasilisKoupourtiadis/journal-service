eeab47a584a9fc244d064d6f59878dbe239a1007
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-26 14:09:49 +0100

    Add PatientService and IPatientService

---
+++ Features/Patients/IPatientService.cs
@@ -0,0 +1,14 @@
+﻿using journal_service.Domain;
+
+namespace journal_service.Features.Patients;
+
+public interface IPatientService
+{
+    Task<ICollection<Patient>> GetAllPatientsAsync();
+
+    Task<Patient> GetPatientAsync(string socialSecurityNumber);
+
+    void AddPatient(Patient patient);
+
+    void RemovePatient(Patient patient);
+}
---
+++ Features/Patients/PatientService.cs
@@ -0,0 +1,24 @@
+﻿using journal_service.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace journal_service.Features.Patients;
+
+public class PatientService : IPatientService
+{
+    private readonly ApplicationContext context;
+
+    public PatientService(ApplicationContext context) =>
+    (this.context) = (context);
+
+    public void AddPatient(Patient patient) =>
+        context.Patients.Add(patient);
+
+    public void RemovePatient(Patient patient) =>
+        context.Patients.Remove(patient);
+
+    public async Task<ICollection<Patient>> GetAllPatientsAsync() =>
+        await context.Patients.ToListAsync();
+
+    public async Task<Patient> GetPatientAsync(string socialSecurityNumber) =>
+        await context.Patients.FirstOrDefaultAsync(x => x.SocialSecurityNumber.Equals(socialSecurityNumber));
+}
