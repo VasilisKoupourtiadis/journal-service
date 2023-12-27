f8e01af4cefce38ee57d1df0f3c688c9bf76f840
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-27 15:23:18 +0100

    Update PatientsController for AddPatient and GetPatientsById

--- Features/Patients/PatientsController.cs
+++ Features/Patients/PatientsController.cs
@@ -1,5 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using static journal_service.Features.Patients.GetAllPatients;
+using static journal_service.Features.Patients.GetPatientById;
+using static journal_service.Features.Patients.AddPatient;
 
 namespace journal_service.Features.Patients;
 
@@ -16,11 +19,33 @@ public class PatientsController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<ICollection<GetAllPatients.PatientResult>>> GetPatientsAsync()
     {
-        var patients = await mediator.Send(new GetAllPatients.GetPatientsQuery());
+        var patients = await mediator.Send(new GetPatientsQuery());
 
         if (patients is null) 
             return NotFound(); 
 
         return Ok(patients);
     }
+
+    [HttpGet("{id:guid}", Name = "GetPatientAsync")]
+    public async Task<ActionResult<GetPatientById.PatientResult>> GetPatientAsync(Guid id)
+    {
+        var query = new GetPatientQuery
+        {
+            Id = id
+        };
+
+        var patient = await mediator.Send(query);
+
+        return Ok(patient);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddPatient([FromBody] AddPatientCommand command)
+    {
+        var patient = await mediator.Send(command);        
+
+        return CreatedAtRoute("GetPatientAsync", new { id = patient.Id }, patient);
+    }
+
 }
