788c83cfa584cc0f61ee225af48292b22c421382
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-27 11:48:34 +0100

    Add GetAllPatients class and mapper profile

---
+++ Features/Patients/GetAllPatients.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using journal_service.ServiceManager;
+using MediatR;
+
+namespace journal_service.Features.Patients;
+
+public class GetAllPatients
+{
+    public class GetPatientsQuery : IRequest<ICollection<PatientResult>> { }
+
+    public class PatientResult
+    {
+        public Guid Id { get; private set; }
+
+        public string FullName { get; private set; } = string.Empty;
+
+        public string SocialSecurityNumber { get; private set; } = string.Empty;
+    }
+
+    public class Handler : IRequestHandler<GetPatientsQuery, ICollection<PatientResult>> 
+    {
+        private readonly IServiceManager serviceManager;
+
+        private readonly IMapper mapper;
+
+        public Handler(IServiceManager serviceManager, IMapper mapper) =>
+            (this.serviceManager, this.mapper) = (serviceManager, mapper);
+
+        public async Task<ICollection<PatientResult>> Handle(GetPatientsQuery query, CancellationToken cancellationToken)
+        {
+            var patients = await serviceManager.Patient.GetAllPatientsAsync();
+
+            var results = mapper.Map<ICollection<PatientResult>>(patients);
+
+            return results;
+        }
+    }
+}
---
+++ Features/Patients/PatientsMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using journal_service.Domain;
+
+namespace journal_service.Features.Patients;
+
+public class PatientsMapper : Profile
+{
+    public PatientsMapper()
+    {
+        CreateMap<Patient, GetAllPatients.PatientResult>()
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.GetFullName()));
+    }
+}
