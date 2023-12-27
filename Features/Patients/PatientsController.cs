e40c601bac129d0ec0b1e37dc1257fb58d6b2ac9
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-27 11:48:55 +0100

    Add PatientsController

---
+++ Features/Patients/PatientsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace journal_service.Features.Patients;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PatientsController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public PatientsController(IMediator mediator) =>
+    (this.mediator) = (mediator);
+
+
+    [HttpGet]
+    public async Task<ActionResult<ICollection<GetAllPatients.PatientResult>>> GetPatientsAsync()
+    {
+        var patients = await mediator.Send(new GetAllPatients.GetPatientsQuery());
+
+        if (patients is null) 
+            return NotFound(); 
+
+        return Ok(patients);
+    }
+}
