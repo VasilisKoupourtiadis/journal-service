afe54a89bc9ee4613c03dcd4f223448c6ba85a64
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2024-01-01 14:45:36 +0100

    Add GetJournal and GetAllJournals classes

---
+++ Features/Journals/Queries/GetAllJournals.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using journal_service.ServiceManager;
+using MediatR;
+
+namespace journal_service.Features.Journals.Queries;
+
+public class GetAllJournals
+{
+    public class GetAllJournalsQuery : IRequest<ICollection<JournalsResult>> { }
+
+    public class JournalsResult
+    {
+        public Guid Id { get; set; }
+
+        public Guid PatientId { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string SocialSecurityNumber { get; set; } = string.Empty;
+    }
+
+    public class Handler : IRequestHandler<GetAllJournalsQuery, ICollection<JournalsResult>>
+    {
+        private readonly IServiceManager serviceManager;
+
+        private readonly IMapper mapper;
+
+        public Handler(IServiceManager serviceManager, IMapper mapper) =>
+            (this.serviceManager, this.mapper) = (serviceManager, mapper);
+
+        public async Task<ICollection<JournalsResult>> Handle(GetAllJournalsQuery request, CancellationToken cancellationToken)
+        {
+            var journals = await serviceManager.Journal.GetAllJournalsAsync();
+
+            var result = mapper.Map<ICollection<JournalsResult>>(journals);
+
+            return result;
+        }
+    }
+}
---
+++ Features/Journals/Queries/GetJournal.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using journal_service.ServiceManager;
+using MediatR;
+
+namespace journal_service.Features.Journals.Queries;
+
+public class GetJournal
+{
+    public class GetJournalQuery : IRequest<JournalResult>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class JournalResult
+    {
+        public Guid Id { get; set; }
+
+        public Guid PatientId { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string SocialSecurityNumber { get; set; } = string.Empty;
+    }
+
+    public class Handler : IRequestHandler<GetJournalQuery, JournalResult>
+    {
+        private readonly IServiceManager serviceManager;
+
+        private readonly IMapper mapper;
+
+        public Handler(IServiceManager serviceManager, IMapper mapper) =>
+            (this.serviceManager, this.mapper) = (serviceManager, mapper);
+
+        public async Task<JournalResult> Handle(GetJournalQuery request, CancellationToken cancellationToken)
+        {
+            var journal = await serviceManager.Journal.GetJournalAsync(request.Id)
+                ?? throw new ArgumentNullException(nameof(request), "Could not find patient");
+
+            var result = mapper.Map<JournalResult>(journal);
+
+            return result;
+        }
+    }
+}
