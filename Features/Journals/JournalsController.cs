a5db723c94f65d525709418a1f85c85dd93dca62
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2024-01-01 14:46:39 +0100

    Add JournalsController and JournalsMapper

---
+++ Features/Journals/JournalsController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using static journal_service.Features.Journals.Queries.GetAllJournals;
+using static journal_service.Features.Journals.Commands.AddJournal;
+using static journal_service.Features.Journals.Queries.GetJournal;
+using journal_service.Features.Journals.Queries;
+
+namespace journal_service.Features.Journals;
+
+[Route("api/[controller]")]
+[ApiController]
+public class JournalsController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public JournalsController(IMediator mediator) =>
+        (this.mediator) = (mediator);
+
+    [HttpGet]
+    public async Task<ActionResult<ICollection<JournalsResult>>> GetJournalsAsync()
+    {
+        var journals = await mediator.Send(new GetAllJournalsQuery());
+
+        if (journals is null) 
+            return NotFound();
+
+        return Ok(journals);
+    }
+
+    [HttpGet("{id:guid}", Name = "GetJournalAsync")]
+    public async Task<ActionResult<GetJournal.JournalResult>> GetJournalAsync(Guid id)
+    {
+        var query = new GetJournalQuery()
+        {
+            Id = id
+        };
+
+        var result = await mediator.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddJournal([FromBody] AddJournalCommand command)
+    {
+        var journal = await mediator.Send(command);
+
+        return CreatedAtRoute("GetJournalAsync", new { id = journal.Id }, journal);
+    }
+}
---
+++ Features/Journals/JournalsMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using journal_service.Domain;
+using journal_service.Features.Journals.Commands;
+using journal_service.Features.Journals.Queries;
+
+namespace journal_service.Features.Journals;
+
+public class JournalsMapper : Profile
+{
+    public JournalsMapper()
+    {
+        CreateMap<Journal, GetAllJournals.JournalsResult>()
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.Patient.GetFullName()))
+            .ForMember(dest => dest.SocialSecurityNumber, opt => opt.MapFrom(src => src.Patient.SocialSecurityNumber));
+        
+        CreateMap<Journal, GetJournal.JournalResult>()
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.Patient.GetFullName()))
+            .ForMember(dest => dest.SocialSecurityNumber, opt => opt.MapFrom(src => src.Patient.SocialSecurityNumber));
+
+        CreateMap<Journal, AddJournal.JournalResult>()
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.Patient.GetFullName()))
+            .ForMember(dest => dest.SocialSecurityNumber, opt => opt.MapFrom(src => src.Patient.SocialSecurityNumber));
+
+    }
+}
